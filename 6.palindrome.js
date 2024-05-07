89c3b6157acfd0f472b734462a2524b51f713f7b
nitish singh <nitishsingh925@gmail.com>
2024-05-07 18:09:05 +0530

    Is Palindrome day 6th

---
+++ 6.palindrome.js
@@ -0,0 +1,28 @@
+/*  
+
+------------------------------------
+Programa: Is Palindrome
+------------------------------------
+
+
+*/
+
+//  write a function to determine whether a given string is a panlindrome or not.
+// a plindrome is a word, phrase, number, or other sequence of characters that reads the same  forward
+// and backward, ignoring spaces. punctuation, and capitaliztion
+
+// the input string may contain letters, digits, spaces, punctuation, and special characters.
+// the function should be case-insesitive
+// ignore spacres,special characters
+// return true and false
+
+const isPalindrome = (str) => {
+  str = str.toLowerCase().replace(/\W/g, "");
+  let rev_str = str.split("").reverse().join("");
+  return str === rev_str;
+};
+
+console.log(isPalindrome("a man, a plan, a canal, panama")); // Output: true
+console.log(isPalindrome("race a car")); // Output: false
+console.log(isPalindrome("racecar")); //Output: true
+console.log(isPalindrome("hello")); // Output: false
