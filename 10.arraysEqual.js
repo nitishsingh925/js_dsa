72fd0e570f5035163c1f088b9bcec8dd40ba7456
nitish singh <nitishsingh925@gmail.com>
2024-05-11 22:16:30 +0530

    Arrays Are Equal  day 10th

---
+++ 10.arraysEqual.js
@@ -0,0 +1,24 @@
+/*  
+
+------------------------------------
+Programa: Arrays Are Equal 
+------------------------------------
+
+
+*/
+
+// write a function arrays are equal that takes two arrays array 1 and array 2
+// as input and returen true if the arrays are equal and also index
+
+const arraysAreEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((curVal, index) => curVal === arr2[index]);
+};
+
+// Exampls
+
+console.log(arraysAreEqual([1, 2, 3], [1, 2, 3])); // true
+console.log(arraysAreEqual([1, 2, 3], [1, 2, 4])); // false
+console.log(arraysAreEqual([], [])); // true
