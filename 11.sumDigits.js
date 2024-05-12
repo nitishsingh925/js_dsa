896c4d5c5593dc2b78f5b7983c3589a4745636cf
nitish singh <nitishsingh925@gmail.com>
2024-05-12 11:53:33 +0530

    Sum of Digits  day 11th

---
+++ 11.sumDigits.js
@@ -0,0 +1,28 @@
+/*  
+
+------------------------------------
+Programa: Sum of Digits 
+------------------------------------
+
+
+*/
+
+// const sumOfDigits = (num) => {
+//   let sum = 0;
+//   num = num.toString();
+//   for (let index = 0; index < num.length; index++) {
+//     sum = sum + parseInt(num[index]);
+//   }
+//   return sum;
+// };
+
+const sumOfDigits = (num) => {
+  return num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc + parseInt(cur), 0);
+};
+
+console.log("Sum of Digits", sumOfDigits(1234)); // output : 10
+console.log("Sum of Digits", sumOfDigits(4321)); // output : 10
+console.log("Sum of Digits", sumOfDigits(123456)); // output : 21
