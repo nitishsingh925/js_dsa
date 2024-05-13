de5a0dc5727b4389b809483ba5fdebec98faf4e6
Nitish Singh <nitishsingh925@gmail.com>
2024-05-13 19:42:14 +0530

    Is Power Of Two

---
+++ 14.isPowerOfTwo.js
@@ -0,0 +1,25 @@
+/*  
+
+------------------------------------
+Programa:  Is Power Of Two 
+------------------------------------
+
+
+*/
+
+// Write a function called isPowerOfTwo that takes an interger num as input and
+// return true if num is a power if two and false otherwise
+
+const isPowerOfTwo = (num) => {
+  if (num <= 0) return false;
+
+  let power = 1;
+  while (power < num) {
+    power *= 2;
+  }
+
+  return power === num;
+};
+
+console.log(isPowerOfTwo(8)); //Output: true
+console.log(isPowerOfTwo(7)); //Output: false
