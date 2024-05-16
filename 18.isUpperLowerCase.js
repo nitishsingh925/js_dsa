6cc8cdc034eed38e4402a77d9e18a631d4db84ca
Nitish Singh <nitishsingh925@gmail.com>
2024-05-16 18:55:53 +0530

    Character is uppercase or lowercase

---
+++ 18.isUpperLowerCase.js
@@ -0,0 +1,18 @@
+/*  
+
+------------------------------------
+Programa: Character is uppercase or lowercase
+------------------------------------
+
+
+*/
+
+// the input char will be a sigle character.
+// the character can be any printable ASCII character.
+// you can assume that the input will always be a string length 1.
+
+const isUpperCase = (c) => c.toUpperCase() === c;
+const isLowerCase = (c) => c.toLowerCase() === c;
+
+console.log(isUpperCase("S")); //Output: true
+console.log(isLowerCase("b")); //Output: true
