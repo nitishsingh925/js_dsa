f68e8b46c53a71f6b9cc663a530caf5881ca04bf
Nitish Singh <nitishsingh925@gmail.com>
2024-05-18 18:03:22 +0530

    Reversed String

---
+++ 20.reverseString.js
@@ -0,0 +1,12 @@
+/*  
+
+------------------------------------
+Programa: Reversed String
+------------------------------------
+
+
+*/
+
+const reverseString = (str) => str.split("").reverse().join("");
+
+console.log(reverseString("Hello World")); //Output:dlroW olleH
