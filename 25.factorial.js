54a29ece16e04953d2693758810a66157c2c7790
Nitish Singh <nitishsingh925@gmail.com>
2024-05-23 16:45:27 +0530

    factorial using recursion

---
+++ 25.factorial.js
@@ -0,0 +1,12 @@
+/*
+------------------------------------
+Programa:  factorial
+------------------------------------
+*/
+
+const factorial = (num) => {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(5));
