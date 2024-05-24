c1c44220a921471dc61dbef8e92676a74cb25a73
Nitish Singh <nitishsingh925@gmail.com>
2024-05-24 17:57:42 +0530

    repeatString

---
+++ 27.repeatString.js
@@ -0,0 +1,11 @@
+/*
+------------------------------------
+Programa:  Repeat String
+------------------------------------
+*/
+
+const repeatString = (s, n) => {
+  return s.repeat(n);
+};
+
+console.log(repeatString("abc", 5)); // Output: "abcabcabcabcabc"
