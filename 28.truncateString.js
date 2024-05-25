58ca69c46cf893a7877d6272144a2a928828dd6f
Nitish Singh <nitishsingh925@gmail.com>
2024-05-25 21:50:18 +0530

    Truncate String

---
+++ 28.truncateString.js
@@ -0,0 +1,15 @@
+/*
+------------------------------------
+Programa:  Truncate String
+------------------------------------
+*/
+
+const truncateString = (str, num) => {
+  if (num <= 0) return str;
+
+  return str.slice(0, num).concat("…");
+
+  //   return num <= 0 ? str : str.slice(0, num).concat("...");
+};
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
