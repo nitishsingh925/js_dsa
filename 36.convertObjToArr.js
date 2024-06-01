3b1ebc2ea3cf12c69716aadd2e22b5c945dd5f0b
Nitish Singh <nitishsingh925@gmail.com>
2024-06-01 19:40:53 +0530

    Convert Object to Array and Vice versa

---
+++ 36.convertObjToArr.js
@@ -0,0 +1,24 @@
+/*  
+
+------------------------------------
+Programa: Convert Object to Array and Vice versa
+------------------------------------
+
+
+*/
+
+const obj = {
+  name: "Nitish",
+  age: 24,
+  city: "alwar",
+};
+
+// Convert Object to Array
+const data = Object.entries(obj);
+
+console.log(data.flat());
+
+// Convert Array to Object
+const obj2 = Object.fromEntries(data);
+
+console.log(obj2);
