d2bdfce34a51856436b008dfaa3e68543b5ea7b2
Nitish Singh <nitishsingh925@gmail.com>
2024-06-01 19:39:34 +0530

    Checking given object is empty or not

---
+++ 35.isEmptyObject.js
@@ -0,0 +1,15 @@
+/*  
+
+------------------------------------
+Programa:  Checking given object is empty or not
+------------------------------------
+
+
+*/
+
+const isEmptyObject = (obj) => Object.keys(obj).length === 0;
+
+console.log(isEmptyObject({})); // Output: true
+console.log(isEmptyObject({ a: 1, b: 2 })); // Output: false
+console.log(isEmptyObject({ keyWithNull: null })); // Output: false
+console.log(isEmptyObject({ keyWithUndefined: undefined })); // Output:false
